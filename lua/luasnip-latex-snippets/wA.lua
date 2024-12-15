069ba9ed24eea2a6f6eb74f3921757f50abb73da
sillybugs <2645033428@qq.com>
2024-12-15 17:28:36 +0800

    secction

--- lua/luasnip-latex-snippets/wA.lua
+++ lua/luasnip-latex-snippets/wA.lua
@@ -16,6 +16,9 @@ function M.retrieve(not_math)
       "$ \\displaystyle  ${1:${TM_SELECTED_TEXT}} $ $0"
     ),
     parse_snippet({ trig = "dm", name = "Block Math" }, "\\[\n\t${1:${TM_SELECTED_TEXT}}\n.\\] $0"),
+    parse_snippet({ trig = "ss1", name = "section" }, "\\section { $1} \n $0 "),
+    parse_snippet({ trig = "ss2", name = "subsection" }, "\\subsection { $1} \n $0 "),
+    parse_snippet({ trig = "ss3", name = "subsubsection" }, "\\subsubsection { $1} \n $0 "),
   }
 end
 
